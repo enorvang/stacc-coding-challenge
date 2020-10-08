26225df5898ac21ed2b8ef5587c2fe9c774ee9a2
enorvang <espen.norvang@gmail.com>
2020-10-08 16:51:08 +0200

    Updates

---
+++ precache-manifest.c99d4f5a3cc828cad2bf57f6ba5f5cb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c27e42dc010932ca17365dbba6e67a4",
+    "url": "/stacc-coding-challenge/index.html"
+  },
+  {
+    "revision": "7dd64d440077f3b5e79e",
+    "url": "/stacc-coding-challenge/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "d86a9ba5dd71793f4f79",
+    "url": "/stacc-coding-challenge/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7dd64d440077f3b5e79e",
+    "url": "/stacc-coding-challenge/static/js/2.bf059eb8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/stacc-coding-challenge/static/js/2.bf059eb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86a9ba5dd71793f4f79",
+    "url": "/stacc-coding-challenge/static/js/main.5156da7d.chunk.js"
+  },
+  {
+    "revision": "f48ed90ef6c57af09ae9",
+    "url": "/stacc-coding-challenge/static/js/runtime-main.e1640626.js"
+  }
+]);
