a9eb1f68c41b6d4a1a4ea55bd6948ab9d2de0ab5
enorvang <espen.norvang@gmail.com>
2020-10-12 13:43:31 +0200

    added som docs

--- src/services/accountServices.js
+++ src/services/accountServices.js
@@ -4,12 +4,17 @@ const baseUrl = "https://fintech-webinar-2020-api.vercel.app/api";
 const token =
   "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik0wTXhNVEEzTVVFeU5rRkZSREZGTnpSRE1VUXdPVVU0TXpBNFF6QkdRVVF6UkVSRE1VSTNSUSJ9.eyJpc3MiOiJodHRwczovL3N0YWNjeC5ldS5hdXRoMC5jb20vIiwic3ViIjoiejQ0RGdzWVFKREtyNTRCcmZvU0x3c2pLWWRtUDRLdWtAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vc3RhY2MuZmludGVjaCIsImlhdCI6MTYwMTIwNTAwNywiZXhwIjoxNjAzNzk3MDA3LCJhenAiOiJ6NDREZ3NZUUpES3I1NEJyZm9TTHdzaktZZG1QNEt1ayIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.fBvgUGcc1zS3eStbdGo19mLC6KqOdMeBdo_xuZEBz9jCzllRfrgqIhPbys5Se2XreGxu5_6oKWlXbqDOvnbuvjTJKnhoO9Aom1meUjqbQgaROeN0hbmPxVDKF-JDtOdZbAWtZv1ds9bWF0zqo9Z7ogicZ0eUi8FnEA2h2I6peVQPL9cJJwSfhjXPW73Ws4e6c0vynnhXLc5BcQgst0iaMZd4n3tdruzP_bgEY5GqbKvJxHjL2KNHh933VZSZdx_7mf4imsgsed2AL1QkIkqj5lvf_niyzrEmOLs_K_rSOZqRzO0c1u9wxrCK7qlryzpv8nz3C3zXfNdnMQHOejOFpQ";
 
+
 const instance = axios.create({
   baseURL: baseUrl,
   timeout: 5000, //if request takes longer, don't complete it.
   headers: { "Authorization" : `Bearer ${token}`},
 });
 
+
+/**
+ * Gets all accounts from api endpoint.
+ */
 const getAll = async () => {
   const response = await instance.get("/accounts");
   return response.data;
--- src/services/coinServices.js
+++ src/services/coinServices.js
@@ -1,16 +1,27 @@
 import axios from "axios";
 const baseUrl = "https://api.coingecko.com/api/v3";
 
+/**
+ * Used to ping the server.
+ */
 const pingServer = async () => {
   const response = await axios.get(`${baseUrl}/ping`);
   return response.data;
 };
 
+/**
+ * Gets a list of coins given a certain currency.
+ * @param {String} currency the currency to use
+ */
 const getAllCoinsForCurrency = async (currency) => {
   const response = await axios.get(`${baseUrl}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`)
   return response.data;
 }
 
+/**
+ * Gets information about a given coin.
+ * @param {*} id id of coin, e.g. "bitcoin"
+ */
 const getCoin = async (id) => {
   const response = await axios.get(`${baseUrl}/coins/${id}`);
   return response.data;
